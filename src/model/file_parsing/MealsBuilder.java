f0dada6f941513ce3200e8decfb09dd6f82cfa34
Richard Deng <rld39@duke.edu>
2022-05-22 23:09:08 -0700

    added json files to data folder

---
+++ src/model/file_parsing/MealsBuilder.java
@@ -0,0 +1,24 @@
+package model.file_parsing;
+
+import model.Food;
+import model.FoodInterface;
+import model.examples.Ingredients;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+public class IngredientsBuilder {
+    private static final String INGREDIENTS_DIRECTORY = "data/ingredients/";
+    private static final String DAIRY_DIRECTORY = "dairy";
+
+    private static final Set<String> INGREDIENTS_DIRECTORIES = Set.of(DAIRY_DIRECTORY);
+
+
+    public final Map<String, FoodInterface> INGREDIENTS = new HashMap<>();
+
+    public IngredientsBuilder() {
+
+    }
+}
